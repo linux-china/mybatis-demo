d6444ce843eecae21f5c5dcb34af87a8868b3dc2
linux_china <linux_china@hotmail.com>
2015-11-30 12:54:21 +0800

    mybatis test application

---
+++ src/test/java/org/mvnsearch/mybatis/MyBatisTestApplication.java
@@ -0,0 +1,16 @@
+package org.mvnsearch.mybatis;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * mybatis test application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+@PropertySource("/application.properties")
+public class MyBatisTestApplication {
+
+}
