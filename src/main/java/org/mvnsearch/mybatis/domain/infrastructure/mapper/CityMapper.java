8ae3bd82c762440e074a6fe3b0fcd54076424186
linux_china <linux_china@hotmail.com>
2015-11-30 12:53:38 +0800

    city mapper

---
+++ src/main/java/org/mvnsearch/mybatis/domain/infrastructure/mapper/CityMapper.java
@@ -0,0 +1,19 @@
+package org.mvnsearch.mybatis.domain.infrastructure.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
+import org.mvnsearch.mybatis.domain.model.City;
+
+/**
+ * city mapper
+ *
+ * @author linux_china
+ */
+public interface CityMapper {
+    @Select("SELECT * from city WHERE STATE = #{state}")
+    @ResultMap("CityResultMap")
+    City findByState(@Param("state") String state);
+
+
+}
