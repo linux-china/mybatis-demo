b51657cac7bee1f4cf9ddca1475a436729b1a23e
linux_china <linux_china@hotmail.com>
2016-03-15 16:23:06 +0800

    add MapperScan

--- src/main/java/org/mvnsearch/mybatis/MyBatisWebApplication.java
+++ src/main/java/org/mvnsearch/mybatis/MyBatisWebApplication.java
@@ -1,5 +1,6 @@
 package org.mvnsearch.mybatis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author linux_china
  */
 @SpringBootApplication
+@MapperScan("org.mvnsearch.mybatis.domain.infrastructure.mapper")
 public class MyBatisWebApplication {
 
     public static void main(String[] args) {
