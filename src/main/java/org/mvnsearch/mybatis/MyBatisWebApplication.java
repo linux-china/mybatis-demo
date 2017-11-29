912b1e556ade16545523d19f23197b2d117965b4
linux_china <linux_china@hotmail.com>
2017-11-29 23:16:26 +0800

    remove mapper scan

--- src/main/java/org/mvnsearch/mybatis/MyBatisWebApplication.java
+++ src/main/java/org/mvnsearch/mybatis/MyBatisWebApplication.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author linux_china
  */
 @SpringBootApplication
-@MapperScan(value = "org.mvnsearch.mybatis.domain.infrastructure.mapper", annotationClass = Mapper.class)
 public class MyBatisWebApplication {
 
     public static void main(String[] args) {
