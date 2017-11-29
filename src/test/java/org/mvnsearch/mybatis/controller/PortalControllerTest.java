8f77669be34f1c6153c6a85642e9131ffd4d6413
linux_china <linux_china@hotmail.com>
2017-11-29 23:16:39 +0800

    portal controller test

---
+++ src/test/java/org/mvnsearch/mybatis/controller/PortalControllerTest.java
@@ -0,0 +1,30 @@
+package org.mvnsearch.mybatis.controller;
+
+import com.github.database.rider.core.api.dataset.DataSet;
+import org.junit.Test;
+import org.mvnsearch.mybatis.MyBatisBaseTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * portal controller test
+ *
+ * @author linux_china
+ */
+@AutoConfigureMockMvc
+public class PortalControllerTest extends MyBatisBaseTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DataSet("/db/dataset/state.xml")
+    public void testGetWishlist() throws Exception {
+        this.mvc.perform(get("/index").accept(MediaType.ALL))
+                .andExpect(status().isOk());
+    }
+}
