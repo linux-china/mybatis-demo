59a06abe0b7f8d985ad8466de57db2c89a873802
linux_china <libing.chen@gmail.com>
2019-10-17 14:36:52 -0700

    Change column name to uppercase

--- src/main/resources/db/migration/V1__city.sql
+++ src/main/resources/db/migration/V1__city.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS city;
 CREATE TABLE city (
-  id         INT PRIMARY KEY AUTO_INCREMENT,
-  name       VARCHAR(64),
-  state      VARCHAR(32),
-  country    VARCHAR(8),
-  updated_at DATETIME,
-  created_at DATETIME
+  ID         INT PRIMARY KEY AUTO_INCREMENT,
+  NAME       VARCHAR(64),
+  STATE      VARCHAR(32),
+  COUNTRY    VARCHAR(8),
+  UPDATED_AT DATETIME,
+  CREATED_AT DATETIME
 ) DEFAULT CHARSET = utf8;
