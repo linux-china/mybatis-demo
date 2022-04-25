6fd288c349878d4c31bf34e24014341a48d2d72c
linux_china <libing.chen@gmail.com>
2022-04-25 11:25:55 -0700

    Add @DBUnit

--- src/test/java/org/mvnsearch/mybatis/MyBatisBaseTest.java
+++ src/test/java/org/mvnsearch/mybatis/MyBatisBaseTest.java
@@ -1,5 +1,6 @@
 package org.mvnsearch.mybatis;
 
+import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.spring.api.DBRider;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,6 +12,7 @@ import org.springframework.test.context.ActiveProfiles;
  */
 @SpringBootTest
 @DBRider
+@DBUnit(cacheConnection = false, leakHunter = true)
 @ActiveProfiles("test")
 public abstract class MyBatisBaseTest {
 }
