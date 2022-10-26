5e9f0a1cc9337db5e8b9585713962b928ed43824
linux_china <libing.chen@gmail.com>
2022-10-26 16:28:31 +0800

    chore: polishing

--- src/main/java/org/mvnsearch/mybatis/controller/PortalController.java
+++ src/main/java/org/mvnsearch/mybatis/controller/PortalController.java
@@ -2,22 +2,20 @@ package org.mvnsearch.mybatis.controller;
 
 import org.mvnsearch.mybatis.domain.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * portal controller
  *
  * @author linux_china
  */
-@Controller
+@RestController
 public class PortalController {
     @Autowired
     public CityRepository cityRepository;
 
     @RequestMapping(path = {"/", "/index"})
-    @ResponseBody
     public String index() {
         return "Hello " + cityRepository.findOne(1L).getName();
     }
