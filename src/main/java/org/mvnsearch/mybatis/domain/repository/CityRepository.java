f292e302bac0b01c412b527d98f6db238f9dc7c3
Libing Chen <libing.chen@gmail.com>
2023-01-19 18:22:51 +0800

    chore: add @Select example

--- src/main/java/org/mvnsearch/mybatis/domain/repository/CityRepository.java
+++ src/main/java/org/mvnsearch/mybatis/domain/repository/CityRepository.java
@@ -1,5 +1,7 @@
 package org.mvnsearch.mybatis.domain.repository;
 
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
 import org.jetbrains.annotations.Nullable;
 import org.mvnsearch.mybatis.domain.model.City;
 
@@ -11,4 +13,8 @@ import org.mvnsearch.mybatis.domain.model.City;
 public interface CityRepository {
     @Nullable
     City findOne(long id);
+
+    @ResultMap("CityResultMap")
+    @Select("select * from CITY where ID = #{id}")
+    City findById(long id);
 }
